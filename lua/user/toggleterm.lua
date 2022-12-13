97d2a2c63b3f8d0609e3d0e303c4fd7a5b5a43ae
Ankit Vashisht <ankitvashisht12@gmail.com>
2022-12-13 21:20:08 +0530

    add node, ncdu toggle in toggleterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -33,8 +33,22 @@ vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local node = Terminal:new({ cmd = "node", hidden = true})
+local ncdu = Terminal:new({ cmd = "ncdu", hidden = true})
+local htop = Terminal:new({ cmd = "htop", hidden = true})
 
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
 
+function _NODE_TOGGLE()
+	node:toggle()
+end
+
+function _NCDU_TOGGLE()
+	ncdu:toggle()
+end
+
+function _HTOP_TOGGLE()
+	htop:toggle()
+end
