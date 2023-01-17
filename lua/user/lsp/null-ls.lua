45c2d705a4c9bc39129a815f5b3674f07c6668f9
Ankit Vashisht <ankitvashisht12@gmail.com>
2023-01-17 17:59:58 +0530

    use eslint daemon for diagnostics

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,11 +14,12 @@ null_ls.setup {
   sources = {
     formatting.prettier.with {
       extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+      -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
-    formatting.black.with { extra_args = { "--fast" } },
+    -- formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
-    formatting.google_java_format,
-    diagnostics.flake8,
+    -- formatting.google_java_format,
+    -- diagnostics.flake8,
+    diagnostics.eslint_d,
   },
 }
